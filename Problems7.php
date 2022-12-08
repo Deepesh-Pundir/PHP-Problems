857620a50b9e5dffe7c8ba9784497648246b89f7
Deepesh Pundir <Deepesh@codedrillinfotech.com>
2022-12-08 10:40:49 +0530

    changes Problems7

--- Problems7.php
+++ Problems7.php
@@ -1,19 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        .black{
+            background-color: black;
+        }
+        .white{
+            background-color: white;
+        }
+    </style>
+</head>
+<body>
 <?php
-echo "<table border=10 cellpadding=30px width:270px height:270px style=border-collapse:collapse>";
+echo "<center>";
+echo "<table border=5 cellpadding=40px  style=border-collapse:collapse>";
+$sum = 0;
 for($i=1 ;$i<=8;$i++)
 {
+    
     echo "<tr>";
     for($j=1 ;$j<=8;$j++)
     {
-        if($j%2==0)
+        $sum = $i+$j;
+        if($sum%2!=0)
         {
-           echo "<td style= width:30px height:30px bgcolor=white></td>"; 
+           echo "<td class='white' ></td>"; 
         }
         else{
-            echo "<td style=width:30px height:30px bgcolor=black></td>";
+            echo "<td class  = 'black'></td>";
         }
+    
+ 
     }
     echo "</tr>";
 }
+
 echo "</table>";
+echo "</center>";
 ?>
+</body>
+</html>
+
+
